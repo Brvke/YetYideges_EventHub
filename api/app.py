2735057d100ab7bb1f8deb2dbf0f97dd35a46eb4
Your Name <your.email@example.com>
2024-07-03 18:27:44 +0300

    checker

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@
 from models import storage
 from api.views import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, render_template, make_response, json, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
